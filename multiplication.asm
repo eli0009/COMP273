da64fd8fd3b8a27eb9e5b6c87e0181df44e11d64
eli0009 <enlai.li@mail.mcgill.ca>
2023-03-12 23:07:32 -0400

    l14 end

--- multiplication.asm
+++ multiplication.asm
@@ -1,6 +1,37 @@
-main:
-    li $t1, 8
+.data
+    nl: .asciiz "\n"
+
+.text
+multiplication:
+    li $t1, 9
     li $t2, 4
     mult $t1, $t2
+    # mfhi gets the high order bits but we don't care about them most of the time
     mflo $a0
     li $v0, 1
+    syscall
+    jal newline
+
+division:
+    li $t1, 9
+    li $t2, 4
+    div $t1, $t2
+    # quotient
+    mflo $a0
+    li $v0, 1
+    syscall
+    jal newline
+    # remainder
+    mfhi $a0
+    li $v0, 1
+    syscall
+    
+exit:
+    li $v0, 10
+    syscall
+
+newline:
+    la $a0, nl
+    li $v0, 4
+    syscall
+    jr $ra
